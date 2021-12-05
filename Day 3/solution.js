266bb71b52ff1b79240d543de9ea8b51d445a8a5
Song Ha <songsongha@gmail.com>
2021-12-04 19:27:54 -0700

    Day 3 - PProblem 2 solution

--- Day 3/solution.js
+++ Day 3/solution.js
@@ -23,8 +23,6 @@ diagnosticReport.forEach(line => {
     })
 })
 
-console.log({ positionCount })
-
 let gamma = '' // most common bits
 let epsilon = '' // least common bits
 
@@ -44,11 +42,54 @@ positionCount.forEach(position =>{
 
 })
 
-console.log({gamma,epsilon})
 const gammaDecimal = parseInt(gamma, 2)
 const epsilonDecimal = parseInt(epsilon, 2)
 
-console.log({gammaDecimal, epsilonDecimal})
 const solution1 = gammaDecimal * epsilonDecimal
-const solution2 = 0
+
+// Problem 2
+
+const mostCommonBit = (report, position, boolean, defaultVal) =>{
+    let mostCommon = 0
+    report.forEach(line =>{
+        const bitArray = line.split('')
+        if (bitArray[position] === '1') {
+            mostCommon += 1
+        } else {
+            mostCommon -= 1
+        }
+    })
+    if (mostCommon < 0 && boolean || mostCommon > 0 && !boolean) {
+        // 0 is the most common bit or we want to know the least common
+        return '0'
+    } else if (mostCommon > 0 && boolean || mostCommon < 0 && !boolean) {
+        // 1 is the most common bit in this position
+        return '1'
+    } else {
+        // equal number of 1s and 0s
+        return defaultVal
+    }
+
+}
+
+const filterReport = (report, boolean, defaultVal) => {
+    let index = 0
+    while (report.length > 1) {
+        const filterVal = mostCommonBit(report, index, boolean, defaultVal)
+        const re = new RegExp(`^.{${index}}${filterVal}`)
+        report = report.filter(str => str.match(re))
+        index++
+    }
+    return report
+    
+}
+
+O2Rate = filterReport(diagnosticReport, true, '1')
+CO2Rate = filterReport(diagnosticReport, false, '0')
+
+const O2Decimal = parseInt(O2Rate[0], 2)
+const CO2Decimal = parseInt(CO2Rate[0], 2)
+
+const solution2 = O2Decimal * CO2Decimal
+
 console.log({solution1, solution2})
