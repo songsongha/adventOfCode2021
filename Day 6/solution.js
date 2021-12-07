6f44fbc3516582255bcf681359bda905def7842d
Song Ha <songsongha@gmail.com>
2021-12-07 16:17:19 -0700

    Day 6 Problem 2 Solution

--- Day 6/solution.js
+++ Day 6/solution.js
@@ -4,7 +4,10 @@
 const fs = require('fs')
 
 // create array of fish timers
-const fishArray = fs.readFileSync('./inputs.txt', 'utf8').split('\,').map(Number)
+const fishInput = fs.readFileSync('./inputs.txt', 'utf8').split('\,').map(Number)
+
+// made this so I wouldn't have to refactor the solution to Problem 1 (it modifies the original input array)
+const fishInput2 = [...fishInput]
 
 const calculateFish = (fishArray, days) =>{
     for (let day = 1; day <= days ; day++) {
@@ -26,4 +29,42 @@ const calculateFish = (fishArray, days) =>{
 }
 
 // Problem 1 Solution
-calculateFish(fishArray, 80)
+calculateFish(fishInput, 80)
+
+// Problem 2 Solution - an issue with memeory at 256 days; need to rethink representation
+
+const calculateFish2 = (fishInput, days) => {
+// create an array where the index represents the timer status and the value is the number of fish with that status
+ let fishStatusArray = new Array(9).fill(0)
+ fishInput.forEach(fishStatus => {
+        fishStatusArray[fishStatus] += 1
+    })
+
+ // increase fishCount
+ const increaseFish = (fishArr) => {
+    const tempArray = new Array(9)
+    for (let timer = 0; timer <= 8 ; timer++) {
+        if (timer === 6){
+            tempArray[timer] =  fishArr[7] + fishArr[0]
+        } else if (timer === 8) {
+            tempArray[8] = fishArr[0]
+        } else {
+            tempArray[timer] = fishArr[timer+1]
+        }
+    }
+    return tempArray
+} 
+
+for (let day = 1; day <= days ; day++) {
+    fishStatusArray = increaseFish(fishStatusArray)
+}
+// total up the fish
+let fishCounter = 0
+fishStatusArray.forEach (fish => {
+    fishCounter += fish
+})
+
+console.log(`There are ${fishCounter} lantern fish after ${days} day(s)`)
+}
+
+calculateFish2(fishInput2, 256)
