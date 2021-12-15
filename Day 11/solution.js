c37e412d452d412e66be73669b098f5049f0ec32
Song Ha <songsongha@gmail.com>
2021-12-15 08:22:51 -0700

    Day 11 Problem 1 Solution

--- Day 11/solution.js
+++ Day 11/solution.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs')
 const input = fs.readFileSync('./inputs.txt', 'utf8').split('\n')
-// console.log({input})
 const energyLevels = input.map((row, rowIndex) => {
     const array = []
     const values = row.split('').map(Number)
@@ -16,31 +15,28 @@ const energyLevels = input.map((row, rowIndex) => {
     })
 })
 
-// console.log({energyLevels})
-
-console.log('statement 1')
 // copied from Day 9 but add in diagonals
 const getAdjacent = (heightMap, current) => {
     const adjacent = []
     const{row, col} = current
 
     // orthoganal
-    if (row >= 1) adjacent.push(heightMap[row-1][col])
-    if (col >= 1) adjacent.push(heightMap[row][col-1])
-    if (row < heightMap.length -1) adjacent.push(heightMap[row+1][col])
-    if (col < heightMap[row].length -1) adjacent.push(heightMap[row][col+1])
+    if (row >= 1) adjacent[adjacent.length] = heightMap[row-1][col]
+    if (col >= 1) adjacent[adjacent.length] = heightMap[row][col-1]
+    if (row < heightMap.length -1) adjacent[adjacent.length] = heightMap[row+1][col]
+    if (col < heightMap[row].length -1) adjacent[adjacent.length] = heightMap[row][col+1]
 
     // diagonal
-    if (row < heightMap.length -1 && col < heightMap[row].length -1) adjacent.push(heightMap[row+1][col+1])
-    if (row < heightMap.length -1 && col >= 1) adjacent.push(heightMap[row+1][col-1])
-    if (row >=1 && col < heightMap[row].length -1) adjacent.push(heightMap[row-1][col+1])
-    if (row >=1 && col >=1) adjacent.push(heightMap[row-1][col-1])
+    if (row < heightMap.length -1 && col < heightMap[row].length -1) adjacent[adjacent.length] = heightMap[row+1][col+1]
+    if (row < heightMap.length -1 && col >= 1) adjacent[adjacent.length] = heightMap[row+1][col-1]
+    if (row >=1 && col < heightMap[row].length -1) adjacent[adjacent.length] = heightMap[row-1][col+1]
+    if (row >=1 && col >=1) adjacent[adjacent.length] = heightMap[row-1][col-1]
 
     return adjacent
 }
 
 let flashCount = 0
-for(let steps = 0 ; steps < 2; steps++){
+for(let steps = 0 ; steps < 100; steps++){
     const queue = []
     // increase all energy levels by 1
     for(let row = 0; row < energyLevels.length; row++) {
@@ -48,15 +44,13 @@ for(let steps = 0 ; steps < 2; steps++){
             energyLevels[row][col].val += 1
             if (energyLevels[row][col].val > 9){
                 // octopus 'flashes'
-                queue.push(energyLevels[row][col])
+                queue[queue.length] = energyLevels[row][col]
             }
         }
     }
     // octopus adjacent to flashes levels are increased
     while(queue.length > 0){
-        console.log({queue})
         const current = queue.shift()
-        console.log({current})
         if (current){
             if (current.val <= 9){
                 energyLevels[current.row][current.col].val +=1
@@ -82,11 +76,5 @@ for(let steps = 0 ; steps < 2; steps++){
         }
     }
 }
-
-console.log('statement3', energyLevels[0][0].val, energyLevels[0][1].val, energyLevels[0][2].val, energyLevels[0][3].val, energyLevels[0][4].val,
- energyLevels[1][0].val, energyLevels[1][1].val, energyLevels[1][2].val, energyLevels[1][3].val, energyLevels[1][4].val,  
- energyLevels[2][0].val, energyLevels[2][1].val, energyLevels[2][2].val, energyLevels[2][3].val, energyLevels[2][4].val,
- energyLevels[3][0].val, energyLevels[3][1].val, energyLevels[3][2].val, energyLevels[3][3].val, energyLevels[3][4].val,
- energyLevels[4][0].val, energyLevels[4][1].val, energyLevels[4][2].val, energyLevels[4][3].val, energyLevels[4][4].val )
 
  console.log({flashCount})
