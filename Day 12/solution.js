49b94b8a6950bfd8db1c334e167b08a846378704
Song Ha <songsongha@gmail.com>
2021-12-26 23:31:11 -0700

    Day 12 Problem 2 Solution

--- Day 12/solution.js
+++ Day 12/solution.js
@@ -1,4 +1,5 @@
 // Problem 1: How many paths are there through the cave that only go through small caves once?
+// Problem 2: If you can enter one of the small caves twice how many paths are there now?
 
 const fs = require('fs')
 const input = fs.readFileSync('./inputs.txt', 'utf8').split('\n')
@@ -14,7 +15,9 @@ const findPathways = (caves, searchParam) => {
     const pathways = []
     caves.forEach (link => {
         if (link.find(cave => cave === searchParam)){
+            if (link.find(cave => cave !== searchParam) !== 'start') {
             pathways[pathways.length] = link.find(cave => cave !== searchParam)
+            }
         }
     })
     return pathways
@@ -40,7 +43,20 @@ let tempRoute = []
                 // count the path
                 endPathCounter++
             } else if (isLowerCase(option) && pathArray.find(cave => cave === option)){
-                // if the path is a small cave and already exists in the route then get rid of it.
+                // if the path is a small cave and already exists in the route then check if there is a duplicate already, if so get rid of it
+                let tempArray = [...pathArray]
+                let isDuplicate = false
+                while (tempArray.length > 0) {
+                    let searchParam = tempArray.pop()
+                    if (isLowerCase(searchParam) && tempArray.find(cave => cave === searchParam)){
+                        // there is a dupliicate 
+                        isDuplicate = true
+                    } 
+                }
+                if (!isDuplicate){
+                    // if there is no duplicate then add it to the array
+                    tempRoute[tempRoute.length] = [...pathArray, option]
+                }
             } else {
                 // add it to the queue
                 tempRoute[tempRoute.length] = [...pathArray, option]
