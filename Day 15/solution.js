8b1616c1e7b440df24f34c8d40a4661d2d2719c5
Song Ha <songsongha@gmail.com>
2021-12-29 23:55:41 -0700

    Day 15 Problem 2 Solution

--- Day 15/solution.js
+++ Day 15/solution.js
@@ -1,4 +1,5 @@
 // Problem 1: What is the loweset total risk from top left to bottom right?
+// Problem 2: The cavern is 5x larger and risk increased/rolls around, what is the new lowest total risk?
 
 const fs = require('fs')
 const input = fs.readFileSync('./inputs.txt', 'utf8').split('\n')
@@ -14,6 +15,33 @@ const cavernMap = input.map((row, rowIndex) => {
         }
     })
 })
+ const getValue = (num) => {
+    if (num > 9) {
+        return num - 9
+    } 
+    return num
+ }
+
+ // create 5x larger cavern
+const cavernMap2 = []
+const cavernRowSize = cavernMap.length
+const cavernColSize = cavernMap[0].length
+for(let rowIndex = 0; rowIndex < cavernRowSize * 5; rowIndex++){
+    const caveCol = []
+    for(let colIndex = 0; colIndex < cavernColSize * 5; colIndex++){
+        const refRow = rowIndex - Math.floor(rowIndex / cavernRowSize) * cavernRowSize
+        const refCol = colIndex - Math.floor(colIndex / cavernColSize) * cavernColSize
+        const value = getValue(cavernMap[refRow][refCol].val + Math.floor(rowIndex / cavernRowSize) + Math.floor(colIndex / cavernColSize))
+        caveCol[caveCol.length] = {
+            val: value,
+            row: rowIndex,
+            col: colIndex,
+            checked: false,
+            totalRisk: Number(0)
+        }
+    }
+    cavernMap2[rowIndex] = caveCol
+}
 
 // taken from Day 9 solution
 const getAdjacent = (cavernMap, current) => {
@@ -35,28 +63,34 @@ const sortQ = (queue) => {
 })
 }
 
-const startPoint = cavernMap[0][0]
-const endPoint = cavernMap[cavernMap.length-1][cavernMap[0].length-1]
-let finished = false
+const findLowestRisk = (cavernMap) => {
 
-// create a priority queue, explore the paths that have the least risk first
-const queue = [startPoint]
+    const startPoint = cavernMap[0][0]
+    const endPoint = cavernMap[cavernMap.length-1][cavernMap[0].length-1]
+    let finished = false
 
-while (!finished){
-    const current = queue.shift()
-    if (current === endPoint) {
-        finished = true
-    } else {
-        const adjacent = getAdjacent(cavernMap, current)
-        for(let i = 0; i < adjacent.length; i++){
-            if(!adjacent[i].checked){
-            adjacent[i].totalRisk = current.totalRisk + adjacent[i].val
-            adjacent[i].checked = true
-            queue.push(adjacent[i])
-            } 
+    // create a priority queue, explore the paths that have the least risk first
+    const queue = [startPoint]
+
+    while (!finished){
+        const current = queue.shift()
+        if (current === endPoint) {
+            finished = true
+        } else {
+            const adjacent = getAdjacent(cavernMap, current)
+            for(let i = 0; i < adjacent.length; i++){
+                if(!adjacent[i].checked){
+                adjacent[i].totalRisk = current.totalRisk + adjacent[i].val
+                adjacent[i].checked = true
+                queue.push(adjacent[i])
+                } 
+            }
+            sortQ(queue)
         }
-        sortQ(queue)
+        
     }
-    
+    console.log('total Risk', endPoint.totalRisk)
 }
-console.log('total Risk', endPoint.totalRisk)
+
+findLowestRisk(cavernMap)
+findLowestRisk(cavernMap2)
