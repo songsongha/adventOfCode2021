219a674803fac72414689b9d48a9abec6b0f4194
Song Ha <songsongha@gmail.com>
2022-01-29 17:15:52 -0700

    Still not working

--- Day 17/solution.js
+++ Day 17/solution.js
@@ -16,66 +16,78 @@ const targetArea = {
 console.log({targetArea})
 
 
-const velocity = {
-    x: targetArea.x[0],
-    y: targetArea.y[0]
-}
+// const velocity = {
+//     x: targetArea.x[0],
+//     y: targetArea.y[0]
+// }
 
 const drag = (xVelocity) => {
     if (xVelocity > 0) return xVelocity - 1
     else if (xVelocity < 0) return xVelocity + 1
     else return 0
 }
+const findMaxY = (velocity) => {
+    // physics equation for finding height of an object based on velocity, discrete heights
+    const maxY = velocity.y * (velocity.y + 1)/ 2 
+    if (maxY === 7626) console.log({velocity})
+    // console.log({maxY})
+    return maxY
+}
 
 // returns maxY value unless probe never enters target area it returns undefined
-const findMaxY = (position, velocity) =>{
+const physics = (position, velocity) =>{
     let withinTarget = false
-    let maxY = position.y
+    const initialVelocity = {...velocity}
     let loopCount = 0
     while (!withinTarget) {
         position.x += velocity.x 
         position.y += velocity.y 
-        if (position.y > maxY) {
-            maxY = position.y
-        }
+        // if (position.y > maxY) {
+        //     maxY = position.y
+        // }
         if (position.x > targetArea.x[0] && position.x < targetArea.x[1] &&
             position.y > targetArea.y[0] && position.y < targetArea.y[1] ) {
                 withinTarget = true
+                return findMaxY(initialVelocity)
             }
         velocity.x = drag(velocity.x)
         velocity.y -= 1 
         loopCount++
         if (loopCount > 400){
-            maxY = undefined
+            return undefined
             break
         }
     }
-    return maxY
 }
 
 const findInitialV = () => {
     let maxY = targetArea.y[0]
-    for ( let i = 0; i < 2 * targetArea.x[1]; i++){
+    for ( let i = 0; i < 3 * targetArea.x[1]; i++){
+        // console.log({i})
         for (let j = -125; j < Math.abs(targetArea.y[1]); j++){
-            const newVelocity = {
+            const velocity = {
                 x: i,
                 y: j
             }
+            
             const position = {
                 x: 0,
                 y: 0
             }
             
-            const yPosition = findMaxY(position, newVelocity)
+            const yPosition = physics(position, velocity)
             if (yPosition && yPosition > maxY){
                 maxY = yPosition
                 console.log({maxY})
-                console.log({newVelocity})
+                console.log({i})
             }
-            newVelocity.y++
         }
     }
     console.log('This is the max Y', maxY)
 }
 
 findInitialV()
+const n = -1 * targetArea.y[0] - 1
+console.log({n})
+const solution = n * (n+1) / 2
+console.log({solution})
